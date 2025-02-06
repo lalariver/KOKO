8a7c9a17bb20ab92e94f1fe392e80d9b78ab4da2
Luke <loveriverain@gmail.com>
2025-02-06 11:43:21 +0800

    Initial Commit

---
+++ KOKOTests/KOKOTests.swift
@@ -0,0 +1,17 @@
+//
+//  KOKOTests.swift
+//  KOKOTests
+//
+//  Created by user on 2025/2/6.
+//
+
+import Testing
+@testable import KOKO
+
+struct KOKOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
